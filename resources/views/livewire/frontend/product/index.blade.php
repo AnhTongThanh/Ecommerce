ee76c31f48a4682322e84f273c4a14c3880263e5
Tống Thành Anh <thanhanhtong.dev@gmail.com>
2023-04-17 15:59:03 +0700

    Anhtt: Checkout

--- resources/views/livewire/frontend/product/index.blade.php
+++ resources/views/livewire/frontend/product/index.blade.php
@@ -50,8 +50,8 @@
                                    </a>
                                 </h5>
                                 <div>
-                                    <span class="selling-price">${{ $productItem->selling_price }}</span>
-                                    <span class="original-price">${{ $productItem->original_price }}</span>
+                                    <span class="selling-price">{{ $productItem->selling_price }} VNĐ</span>
+                                    <span class="original-price">{{ $productItem->original_price }} VNĐ</span>
                                 </div>
                             </div>
                         </div>
